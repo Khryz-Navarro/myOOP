f10dd4114eb4580faca5978e73bdab1ed1792f46
Khryz-Navarro <s.konavarro@usm.edu.ph>
2024-05-12 11:24:34 +0800

    commit

---
+++ JavaGame/src/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args) {
+
+        Character c1 = new Character();
+
+        c1.setName("Ken");
+        c1.charStats();
+        c1.charAttack();
+        c1.charStats();
+
+
+    }
+}
